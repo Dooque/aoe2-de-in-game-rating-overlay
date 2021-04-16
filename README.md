3b746f6b19aaed9ea025569813cabf0ccb05e208
Gonzalo Bonigo <gonzalobonigo@gmail.com>
2021-04-16 12:43:21 -0300

    Upload the first version of the program.

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# aoe2-de-mp-ratings
+# Age of Empires II DE - Multiplayer Ratings
+
+## Download
+
+You can download the latest release from [here]().
+
+## Introduction
+
+This tool shows the RM 1v1 ELO and RM TG ELO for all players in a multiplayer game.
+
+It currently works only for "Random Map" games.
+
+You can drag the text anywhere in the in the screen.
+
+The 1v1 ELO is shown between *[ ]* and the TG ELO is shown between *( )*.
+
+![](./res/picture1.png)
+![](./res/picture2.png)
+
+## Installation & Configuration
+
+The program requires no installation. Just extract the ZIP file and run the `aoe2de-mp-ratings.exe` file.
+
+The only required configuration you need is to write your [AoE2.net](https://aoe2.net) profile ID into the `AOE2NET_PROFILE_ID.txt` file.
+
+### How to close the program?
+
+After have moved the text around the window and without click anywhere else press `Alt+F4`.
+
+### How do I get my AoE2.net profile ID?
+
+Go to https://aoe2.net/.
+
+Click on "Leaderboards" and pick "Random Map":
+
+![](./res/picture3.png)
+
+On the search section enter your Steam profile name. Once you see yourself in the table click on your name:
+
+> NOTE: If you don't appear in the list it is because you haven't play at least 10 ranked games. You need to play 10 ranked games to have an AoE2.net profile ID.
+
+![](./res/picture4.png)
+
+Click on the "Profile" button at the bottom right of the window:
+
+![](./res/picture5.png)
+
+Then you will see the profile ID in the URL section:
+
+![](./res/picture6.png)
+
+Copy and paste it into the `AOE2NET_PROFILE_ID.txt` file.
+
+And that's it you can run the `aoe2de-mp-ratings.exe` file.
+
+## What's next?
+
+1. Be able to change the text color, font and size from a configuration file.
+2. Be able to set a background with solid color from a configuration file.
+3. Show the text with the current color of each player in the game.
+4. Show the text with the "self (blue) / ally (yellow) / enemy (red)" color mode.
+5. Show extended player information when the mouse goes over the player name.
+6. Make configurable the refresh time.
+
+## CHANGELOG
+
+### v0.0.1
+
+* Fetch the latest game of the AoE2.net profile ID saved in the `AOE2NET_PROFILE_ID.txt` file.
+* Fetch all the players from the latest game.
+* Fetch the following information from each player:
+  * Random Map 1v1 Rating.
+  * Random Map 1v1 number of wins.
+  * Random Map 1v1 number of losses.
+  * Random Map 1v1 streak.
+  * Random Map Team Game Rating.
+  * Random Map Team Game number of wins.
+  * Random Map Team Game number of losses.
+  * Random Map Team Game streak.
+* Show some of the information in a transparent window of fixed text font (Arial), size (14) and color. (white).
+* The program fetch for a new game every a fixed amount of time (10 seconds).
---
+++ aoe2de-mp-ratings.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+#
+# python -m pysimplegui-exemaker.pysimplegui-exemaker
+#
+
+import os
+import requests
+import sys
+import threading
+import time
+
+import PySimpleGUI as sg
+
+player_info_template = [
+    '{name} ({tgelo}) [{elo}] P{numer}',
+    'P{numer} [{elo}] ({tgelo})  {name}'   
+]
+
+title = 'Age of Empires II DE - Multyplayer Ratings'
+
+t1p1_info = sg.Text('.' * 40, pad=((0,0),(0,0)), background_color='#000000', justification='right', font=('Arial', 12))
+t1p2_info = sg.Text('.' * 40, pad=((0,0),(0,0)), background_color='#000000', justification='right', font=('Arial', 12))
+t1p3_info = sg.Text('.' * 40, pad=((0,0),(0,0)), background_color='#000000', justification='right', font=('Arial', 12))
+t1p4_info = sg.Text('.' * 40, pad=((0,0),(0,0)), background_color='#000000', justification='right', font=('Arial', 12))
+t2p1_info = sg.Text('.' * 40, pad=((0,0),(0,0)), background_color='#000000', justification='left', font=('Arial', 12))
+t2p2_info = sg.Text('.' * 40, pad=((0,0),(0,0)), background_color='#000000', justification='left', font=('Arial', 12))
+t2p3_info = sg.Text('.' * 40, pad=((0,0),(0,0)), background_color='#000000', justification='left', font=('Arial', 12))
+t2p4_info = sg.Text('.' * 40, pad=((0,0),(0,0)), background_color='#000000', justification='left', font=('Arial', 12))
+
+team_players_info = [
+    [
+        t1p1_info,
+        t1p2_info,
+        t1p3_info,
+        t1p4_info,
+    ],
+    [
+        t2p1_info,
+        t2p2_info,
+        t2p3_info,
+        t2p4_info,
+    ],
+]
+
+team1_column = [
+    [t1p1_info],
+    [t1p2_info],
+    [t1p3_info],
+    [t1p4_info],
+]
+
+separator_column = []
+
+team2_column = [
+    [t2p1_info],
+    [t2p2_info],
+    [t2p3_info],
+    [t2p4_info],
+]
+
+layout = [
+    [
+        sg.Column(team1_column, background_color='#000000', vertical_alignment='top', element_justification='right'),
+        sg.VSeparator(),
+        sg.Column(team2_column, background_color='#000000', vertical_alignment='top', element_justification='right'),
+    ]
+]
+
+def fetch_data():
+
+    r = requests.get('https://aoe2.net/api/strings?game=aoe2de&language=en')
+    strings = r.json()
+
+    match_uuid = ''
+    while True:
+        f = open('./AOE2NET_PROFILE_ID.txt', 'r')
+        AOE2NET_PROFILE_ID = int(f.read())
+        f.close()
+
+        # Get Last/Current match.
+        r = requests.get('https://aoe2.net/api/player/lastmatch?game=aoe2de&profile_id={}'.format(AOE2NET_PROFILE_ID))
+        match = r.json()
+
+        if match_uuid != match['last_match']['match_uuid']:
+            match_uuid = match['last_match']['match_uuid']
+            game_type_id = match['last_match']['game_type']
+            game_type_string = [x['string'] for x in strings['game_type'] if x['id'] == game_type_id].pop()
+            map_type_id = match['last_match']['map_type']
+            map_type_string = [x['string'] for x in strings['map_type'] if x['id'] == map_type_id].pop()
+            
+            players_team1 = [player for player in match['last_match']['players'] if player['team'] == 1]
+            players_team2 = [player for player in match['last_match']['players'] if player['team'] == 2]
+
+            t1p1_info.Update(value='')
+            t1p2_info.Update(value='')
+            t1p3_info.Update(value='')
+            t1p4_info.Update(value='')
+            t2p1_info.Update(value='')
+            t2p2_info.Update(value='')
+            t2p3_info.Update(value='')
+            t2p4_info.Update(value='')
+
+            team_number = 0
+            for team in (players_team1, players_team2):
+                player_numer = 0
+                for player in team:
+                    profile_id = player['profile_id']
+                    rating_1v1 = requests.get('https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=3&count=1&profile_id={}'.format(profile_id))
+                    rating_tg = requests.get('https://aoe2.net/api/player/ratinghistory?game=aoe2de&leaderboard_id=4&count=1&profile_id={}'.format(profile_id))
+
+                    civ = [ x['string'] for x in strings['civ'] if x['id'] == player['civ']]
+                    if civ:
+                        civ = civ.pop()
+                    else:
+                        civ = 'N/A'
+
+                    rating_1v1 = rating_1v1.json()
+                    if (len(rating_1v1) == 0):
+                        games_1v1 = 'N/A  '
+                        rating_1v1 = {'rating':'N/A', 'num_wins':'N/A', 'num_losses':'N/A', 'streak':'N/A'}
+                    else:
+                        rating_1v1 = rating_1v1.pop()
+                        games_1v1 = str(rating_1v1['num_wins'] + rating_1v1['num_losses'])
+                        games_1v1 = games_1v1 + " " * (4 - len(games_1v1))
+                        rating_1v1['rating'] = str(rating_1v1['rating'])
+                        rating_1v1['rating'] = rating_1v1['rating'] + " " * (4 - len(rating_1v1['rating']))
+                        rating_1v1['num_wins'] = str(rating_1v1['num_wins'])
+                        rating_1v1['num_wins'] = rating_1v1['num_wins'] + " " * (4 - len(rating_1v1['num_wins']))
+                        rating_1v1['num_losses'] = str(rating_1v1['num_losses'])
+                        rating_1v1['num_losses'] = rating_1v1['num_losses'] + " " * (4 - len(rating_1v1['num_losses']))
+                        rating_1v1['streak'] = str(rating_1v1['streak'])
+                        rating_1v1['streak'] = rating_1v1['streak'] + " " * (4 - len(rating_1v1['streak']))
+
+                    rating_tg = rating_tg.json()
+                    if (len(rating_tg) == 0):
+                        games_tg = 'N/A  '
+                        rating_tg = {'rating':'N/A', 'num_wins':'N/A', 'num_losses':'N/A', 'streak':'N/A'}
+                    else:
+                        rating_tg = rating_tg.pop()
+                        games_tg = str(rating_tg['num_wins'] + rating_tg['num_losses'])
+                        games_tg = games_tg + " " * (4 - len(games_tg))
+                        rating_tg['rating'] = str(rating_tg['rating'])
+                        rating_tg['rating'] = rating_tg['rating'] + " "*(4 - len(rating_tg['rating']))
+                        rating_tg['num_wins'] = str(rating_tg['num_wins'])
+                        rating_tg['num_wins'] = rating_tg['num_wins'] + " "*(4 - len(rating_tg['num_wins']))
+                        rating_tg['num_losses'] = str(rating_tg['num_losses'])
+                        rating_tg['num_losses'] = rating_tg['num_losses'] + " "*(4 - len(rating_tg['num_losses']))
+                        rating_tg['streak'] = str(rating_tg['streak'])
+                        rating_tg['streak'] = rating_tg['streak'] + " "*(4 - len(rating_tg['streak']))
+
+                    text = player_info_template[team_number].format(
+                        numer=player['color'],
+                        name=player['name'],
+                        tgelo=rating_tg['rating'],
+                        elo=rating_1v1['rating'],
+                    )
+
+                    team_players_info[team_number][player_numer].Update(value=text)
+
+                    player_numer += 1
+                team_number += 1
+        time.sleep(10)
+
+if __name__ == '__main__':
+
+    window = sg.Window( title,
+                        layout,
+                        no_titlebar=True,
+                        keep_on_top=True,
+                        grab_anywhere=True,
+                        background_color='black',
+                        #transparent_color='black',
+                        alpha_channel=1)
+
+    threading.Thread(target=fetch_data, daemon=True).start()
+
+    while True:
+       event, values = window.read()
+       if event in (sg.WIN_CLOSED, 'Exit'):
+            break
+
+    window.close()
